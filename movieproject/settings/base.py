2b197f4e8327074d6f8570cc9ee3eb52a2cffda3
iamdeveloper.me <iamdeveloper.me@gmail.com>
2020-06-05 14:47:02 +0530

    Update file

--- movieproject/settings/base.py
+++ movieproject/settings/base.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'movieproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'movie',
+        'USER':     'postgres',
+        'PASSWORD': 'postgres',
+        'HOST':     'localhost',
+        'PORT':     5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -133,7 +143,7 @@ STATICFILES_DIRS = [
 ]
 
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
